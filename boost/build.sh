7df9204252fcbe96cfd5ad5c14ba0bc93956d1cc
pradal <christophe.pradal@inria.fr>
2017-06-27 17:58:55 +0100

    Update with newer deploy and sconsx versions

--- boost/build.sh
+++ boost/build.sh
@@ -22,53 +22,36 @@ if [ "$(uname)" == "Darwin" ]; then
     LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
     LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
 
-    ./bootstrap.sh \
-        --prefix="${PREFIX}" \
-        --with-python="${PYTHON}" \
-        --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}" \
-        --with-icu="${PREFIX}" \
-        | tee bootstrap.log 2>&1
-
-    ./b2 -q \
-        variant=release \
-        address-model=64 \
-        architecture=x86 \
-        debug-symbols=off \
-        threading=multi \
-        link=static,shared \
-        toolset=clang \
-        include="${INCLUDE_PATH}" \
-        cxxflags="${CXXFLAGS}" \
-        linkflags="${LINKFLAGS}" \
-        -j"$(sysctl -n hw.ncpu)" \
-        install | tee b2.log 2>&1
-fi
-
-if [ "$(uname)" == "Linux" ]; then
+    TOOLSET=clang
+elif [ "$(uname)" == "Linux" ]; then
     CXXFLAGS="${CXXFLAGS} -std=c++11"
     LINKFLAGS="${LINKFLAGS} -std=c++11 -L${LIBRARY_PATH}"
 
-    ./bootstrap.sh \
-        --prefix="${PREFIX}" \
-        --with-python="${PYTHON}" \
-        --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}" \
-        --with-icu="${PREFIX}" \
-        | tee bootstrap.log 2>&1
-
-    ./b2 -q \
-        variant=release \
-        address-model="${ARCH}" \
-        architecture=x86 \
-        debug-symbols=off \
-        threading=multi \
-        runtime-link=shared \
-        link=static,shared \
-        toolset=gcc \
-        python="${PY_VER}" \
-        include="${INCLUDE_PATH}" \
-        cxxflags="${CXXFLAGS}" \
-        linkflags="${LINKFLAGS}" \
-        --layout=system \
-        -j"${CPU_COUNT}" \
-        install | tee b2.log 2>&1
+    TOOLSET=gcc
 fi
+
+./bootstrap.sh \
+    --prefix="${PREFIX}" \
+    --with-libraries=all \
+    --with-python="${PYTHON}" \
+    --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}" \
+    --with-icu="${PREFIX}" \
+    2>&1 | tee bootstrap.log
+
+./b2 -q \
+    variant=release \
+    address-model="${ARCH}" \
+    architecture=x86 \
+    debug-symbols=off \
+    threading=multi \
+    runtime-link=shared \
+    link=static,shared \
+    toolset="${TOOLSET}" \
+    include="${INCLUDE_PATH}" \
+    python="${PY_VER}" \
+    cxxflags="${CXXFLAGS}" \
+    linkflags="${LINKFLAGS}" \
+    --layout=system \
+    -j"${CPU_COUNT}" \
+    install 2>&1 | tee b2.log
+
